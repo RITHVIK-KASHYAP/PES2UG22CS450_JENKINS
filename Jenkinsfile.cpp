1dc556939ceb8352934082e41d6b6ffdcf941fc7
RITHVIK KASHYAP B S <rithvik052003@gmail.com>
2025-03-13 23:53:58 +0530

    Create Jenkinsfile.cpp
    jenkinsfile1

---
+++ Jenkinsfile.cpp
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                shh 'g++ -o output PES2UG22CS450.cpp' // Intentional typo in 'sh'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh './output'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'Deploying application...'
+            }
+        }
+    }
+
+    post {
+        failure {
+            echo 'Pipeline failed'
+        }
+    }
+}
